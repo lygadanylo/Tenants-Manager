cfe9fed345aaa291081cf10ce5f0c162d82f36b3
Danylo Lyha <danylolyha@HRK1-LMC-F73787.local>
2022-01-04 12:16:05 +0200

    fixed avatar

--- src/components/UserAvatar/index.tsx
+++ src/components/UserAvatar/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function stringAvatar(name: string) {
   return {
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1]}`,
   };
 }
 
